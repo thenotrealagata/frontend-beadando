79d5481ac3fcb21c7f951ebdeec2e7c04bb3170c
Fábián Ágota <agota.fabian16@gmail.com>
2024-04-18 23:15:50 +0200

    initialize react, add tailwind

---
+++ berkalkulator/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
